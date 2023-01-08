e7188e64eefe9df2653df2dd74c217856d2dbe1f
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-01-08 21:06:42 +0900

    [Silver IV] Title: 덱, Time: 76 ms, Memory: 34136 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,48 @@
+from collections import deque
+import sys
+
+num = int(sys.stdin.readline())
+
+dq = deque()
+
+for i in range(num):
+    X = list(sys.stdin.readline().split())
+
+    if(X[0] == "push_front"):
+        dq.appendleft(X[1])
+
+    elif(X[0] == "push_back"):
+        dq.append(X[1])
+
+    elif(X[0] == "pop_front"):
+        if(len(dq)==0):
+            print("-1")
+        else:
+            print(dq.popleft())
+
+    elif(X[0] == "pop_back"):
+        if(len(dq)==0):
+            print("-1")
+        else:
+            print(dq.pop())
+
+    elif(X[0] == "size"):
+        print(len(dq))
+
+    elif(X[0] == "empty"):
+        if(len(dq)==0):
+            print("1")
+        else:
+            print("0")
+
+    elif(X[0] == "front"):
+        if(len(dq)==0):
+            print("-1")
+        else:
+            print(dq[0])
+
+    elif(X[0] == "back"):
+        if(len(dq)==0):
+            print("-1")
+        else:
+            print(dq[-1])
