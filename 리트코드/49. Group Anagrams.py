7d29cc44e15c17168117f1c9c38b0790c6702cd3
Chaeyeon1 <121411603+Chaeyeonchae@users.noreply.github.com>
2023-01-20 14:22:33 +0900

    49. Group Anagrams

---
+++ 리트코드/49. Group Anagrams.py
@@ -0,0 +1,18 @@
+# strs = ["eat","tea","tan","ate","nat","bat"]
+# set_strs_list = set(strs)
+
+# for i in strs:
+#     # answer.append([i]) 
+#     print(set_strs_list)
+#     if("".join(sorted(i)) in set_strs_list): # [[ate], [bat], [cat]]
+#         print(set_strs_list.index(''.join(sorted(i))))
+
+
+# # print(answer)
+
+a = [["abc"], ["bca"], ["bcc"]]
+a[0].append("bcc")
+
+# print(a.find("abc")) 
+
+print(a[0][1])
