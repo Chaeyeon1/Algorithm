300047627c83165a95e279db3a161052cdf9f98d
chaeyeon1 <slide10245@naver.com>
2023-03-04 23:49:32 +0900

    hide 짠 코드

--- 백준/Silver/hide.py
+++ 백준/Silver/hide.py
@@ -2,21 +2,20 @@ from collections import deque
 N, K = list(map(int, input().split()))
 
 def bfs(start):
-    global visited
-    visited = [0 for _ in range(100001)]
+    visited = [0 for _ in range(200001)]
     
     deq = deque()
     deq.append((start, 0))
     while len(deq)!=0:
         current, second = deq.popleft()
-
         if(current == K):
             print(second)
             return
 
-        if(0<current and current<100000 and visited[current]==0):
-            visited[current] = 1
-            deq.append((current-1, second+1))
-            deq.append((current+1, second+1))
-            deq.append((current*2, second+1))
+        if(0<=current and current<=200000 and visited[current]==0):
+                visited[current] = 1
+                deq.append((current-1, second+1))
+                deq.append((current+1, second+1))
+                deq.append((current*2, second+1))
+
 bfs(N)
