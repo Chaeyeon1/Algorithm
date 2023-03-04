fa0fe2c22a83178a545f3911ec656961c674d87d
chaeyeon1 <slide10245@naver.com>
2023-03-04 19:49:12 +0900

    조건식 잘못 세움

--- 백준/Silver/hide.py
+++ 백준/Silver/hide.py
@@ -2,7 +2,7 @@ from collections import deque
 N, K = list(map(int, input().split()))
 
 def bfs(start):
-    # global visited
+    global visited
     visited = [0 for _ in range(100001)]
     
     deq = deque()
@@ -10,14 +10,13 @@ def bfs(start):
     while len(deq)!=0:
         current, second = deq.popleft()
 
-        if(0<current<100000 and visited[current]==0):
-            if(current+1==K or current-1==K or current*2==K):
-                print(second+1)
-                return
+        if(current == K):
+            print(second)
+            return
 
-            else:
-                visited[current] = 1
-                deq.append((current-1, second+1))
-                deq.append((current+1, second+1))
-                deq.append((current*2, second+1))
+        if(0<current and current<100000 and visited[current]==0):
+            visited[current] = 1
+            deq.append((current-1, second+1))
+            deq.append((current+1, second+1))
+            deq.append((current*2, second+1))
 bfs(N)
