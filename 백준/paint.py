a9bce39e48dd5582e193cb83d7a611d7d5f69f84
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-03-06 21:26:04 +0900

    정답

--- 백준/paint.py
+++ 백준/paint.py
@@ -1,16 +1,20 @@
 def solution(n, m, section):
     answer = 0
     
-    while(len(section)!=0):
-        start = section[0]
+    arr = [0 for _ in range(n+1)]
+    for i in section:
+        arr[i] = 1
     
-        answer += 1
-        for i in range(start, start+m):
-            if(i>n):
-                break
-            if i in section:
-                section.remove(i)
-    
+    paint = 0
+
+    while(paint<=n):
+        if(arr[paint] == 1):
+            answer += 1
+
+            paint+= m
+        else:
+            paint += 1
+   
     return answer
 
 print(solution(19, 4, [2,5,8,12,15,19]))
