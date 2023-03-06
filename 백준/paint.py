c8b0a75ce63d8890489b11867bf0ed8f1184be26
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-03-06 21:08:30 +0900

    쓸 데 없는 코드 뺌

--- 백준/paint.py
+++ 백준/paint.py
@@ -1,28 +1,15 @@
 def solution(n, m, section):
     answer = 0
     
-    # n = 총 개수               8
-    # m = 페인트 칠할 수 있는 개수 4
-    # section = 몇 번이 비어있는지 2,3,6
-
-    # 제일 작은 숫자로 가서, +m만큼을 탐색
-    # 있으면 count+1하고 배열에서 있는 숫자들을 다 뺀다.
-    # 그리고 그 다음 가장 작은 숫자로 간다. 반복
-
     while(len(section)!=0):
-        start = min(section)
-        end = max(section)
+        start = section[0]
     
         answer += 1
-        for i in range(start, start+m): # 예외처리 해줘야 할듯
+        for i in range(start, start+m):
+            if(i>n):
+                break
             if i in section:
                 section.remove(i)
-
-        if(len(section) != 0):
-            answer += 1
-            for i in range(end-m+1, end+1): # 예외처리 해줘야 할듯
-                if i in section:
-                    section.remove(i)
     
     return answer
 
