ece79ebd206883a3b4ee22040dbd3f6793fce4b7
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-05-12 21:27:21 +0900

    [unrated] Title: 달리기 경주, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -1,14 +1,21 @@
 def solution(players, callings):
-    answer = []
-    answer = players[:]
+    answer = players
     
-    for i in range(len(callings)):
-        currentCallingIndex = answer.index(callings[i])
-        previousCallingIndex = currentCallingIndex - 1
+    dic = {player: i+1 for i,player in enumerate(players) }
+    
+    for call in callings:
+        # kai를 부름 -> 4 -> 3
+        dic[call] = dic[call] - 1
 
-        # 서로 변경해줌
-        temp = answer[previousCallingIndex]
-        answer[previousCallingIndex] = answer[currentCallingIndex]
-        answer[currentCallingIndex] = temp
+        # "poe"
+        loser = answer[dic[call]-1]
+
+        # "poe의 등수"
+        dic[loser] = dic[loser] + 1
+
+        # temp에서 poe와 kai의 자리를 바꿈
+        # 2번 인덱스, 3번 인덱스
+        answer[dic[call]-1], answer[dic[call]] = answer[dic[call]], answer[dic[call]-1]
+    
     
     return answer
