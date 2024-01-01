17c89ed41ec4dd23452e365e24646f68ad70be34
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2024-01-01 21:49:50 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -4,8 +4,6 @@ const filePath =
 
 const input = fs.readFileSync(filePath, "utf-8").split(" ").map(Number);
 
-const answer = input.reduce(
-  (accumulator, currentValue) => accumulator + currentValue,
-  0
-);
+const answer = input[0] + input[1];
+
 console.log(answer);
