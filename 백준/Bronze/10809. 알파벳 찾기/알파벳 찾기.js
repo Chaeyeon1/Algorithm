eb24e536dc408765fb4b854031ec48f04ab98f99
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2024-01-01 23:40:20 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 136 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+
+const input = fs.readFileSync(filePath, "utf-8");
+
+const result = [];
+
+for (let i = 97; i <= 122; i++) {
+  result.push(input.indexOf(String.fromCharCode(i)));
+}
+
+console.log(result.join(" "));
